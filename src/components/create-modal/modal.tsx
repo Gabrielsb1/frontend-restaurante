f9f9a24ead72446017788f64125fcdb1529f3730
Gabrielsb1 <joaogabrielmoto2015@gmail.com>
2023-05-17 03:24:06 -0300

    update beta

--- src/components/create-modal/modal.tsx
+++ src/components/create-modal/modal.tsx
@@ -4,66 +4,104 @@ import { FoodData } from '../interface/FoodData';
 import styles from "./modal.module.css";
 
 interface InputProps {
-    label: string,
-    value: string | number,
-    updateValue(value: any): void
+  label: string,
+  value: string | number,
+  updateValue(value: any): void
 }   
 
 interface ModalProps {
-    closeModal(): void
+  closeModal(): void
 }
 
 const Input = ({ label, value, updateValue }: InputProps) => {
-    return (
-        <>
-            <label>{label}</label>
-            <input value={value} onChange={event => updateValue(event.target.value)}></input>
-        </>
-    )
+  return (
+    <>
+      <label>{label}</label>
+      <input value={value} onChange={event => updateValue(event.target.value)}></input>
+    </>
+  )
 }
 
 export function CreateModal({ closeModal }: ModalProps){
-    const [title, setTitle] = useState("");
-    const [price, setPrice] = useState(0);
-    const [urlimg, setImage] = useState("");
-    const { mutate, isSuccess, isLoading } = useFoodDataMutate();
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState(0);
+  const [urlimg, setImage] = useState("");
+  const [editMode, setEditMode] = useState(false);
+  const [id, setId] = useState(0);
+  const { mutate, isSuccess, isLoading } = useFoodDataMutate();
 
-    const submit = () => {
-        const foodData: FoodData = {
-            title, 
-            price,
-            urlimg
-        }
-        mutate(foodData)
+  const submit = () => {
+    const foodData: FoodData = {
+      title, 
+      price,
+      urlimg
     }
+    mutate(foodData)
+  }
 
-    useEffect(() => {
-        if(!isSuccess) return 
-        closeModal();
-    }, [isSuccess])
+  useEffect(() => {
+    if(!isSuccess) return 
+    closeModal();
+  }, [isSuccess])
 
-    const handleGoBack = () => {
-        closeModal();
-    }
+  const handleGoBack = () => {
+    closeModal();
+  }
 
-    return(
-        <div className={styles.modal_overlay}>
-            <div className={styles.modal_body}>
-                <h2>Cadastre um novo item no cardápio</h2>
-                <form className={styles.input_container}>
-                    <Input label="title" value={title} updateValue={setTitle}/>
-                    <Input label="price" value={price} updateValue={setPrice}/>
-                    <Input label="image" value={urlimg} updateValue={setImage}/>
-                </form>
-                <div className={styles.button_container}>
-                    <button onClick={submit} className={`${styles.btn_secondary} ${styles.btn_submit}`}>
-                        {isLoading ? 'postando...' : 'Postar'}
-                    </button>
-                    <button onClick={handleGoBack} className={`${styles.btn_secondary} ${styles.btn_back}`}>
-                        Voltar
-                    </button>
-                </div>
-            </div>
-        </div>
-    )
+  const handleEdit = () => {
+    setEditMode(true);
+  }
+
+  const handleDelete = () => {
+    // Lógica de exclusão aqui
+  }
+
+  const handleEditSubmit = () => {
+    // Lógica de edição com o ID aqui
+  }
+
+  return(
+    <div className={styles.modal_overlay}>
+      <div className={styles.modal_body}>
+        <h2>Cadastre um novo item no cardápio</h2>
+        <form className={styles.input_container}>
+          {!editMode ? (
+            <>
+              <Input label="title" value={title} updateValue={setTitle}/>
+              <Input label="price" value={price} updateValue={setPrice}/>
+              <Input label="image" value={urlimg} updateValue={setImage}/>
+            </>
+          ) : (
+            <>
+              <Input label="ID" value={id} updateValue={setId}/>
+            </>
+          )}
+          <div className={styles.button_container}>
+            {!editMode ? (
+              <>
+                <button onClick={submit} className={`${styles.btn_secondary} ${styles.btn_submit}`}>
+                  {isLoading ? 'Postando...' : 'Postar'}
+                </button>
+                <button onClick={handleGoBack} className={`${styles.btn_secondary} ${styles.btn_back}`}>
+                  Voltar
+                </button>
+                <button onClick={handleEdit} className={`${styles.btn_secondary} ${styles.btn_edit}`}>
+                  Editar
+                </button>
+              </>
+            ) : (
+              <>
+                <button onClick={handleGoBack} className={`${styles.btn_secondary} ${styles.btn_submit}`}>
+                Voltar
+                </button>
+                <button onClick={handleDelete} className={`${styles.btn_secondary} ${styles.btn_delete}`}>
+                  Deletar
+                </button>
+              </>
+            )}
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
